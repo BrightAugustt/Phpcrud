8900233b41583cd49cd8d141d15a2a5e9ff086ab
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-24 21:46:24 +0100

    changes

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
  <?php
-$connection = mysqli_connect("us-cdbr-east-06.cleardb.net","b8381e8687357a","246b0cb1","heroku_c41210addaccbb7");
+$connection = mysqli_connect("us-cdbr-east-06.cleardb.net","b9e2f55baf0395","e3e1ccc3","heroku_28ad1c02dc3433c");
 
 
 // Check connection
@@ -8,5 +8,5 @@ if ($connection->connect_error) {
     echo "Unsuccessful connnection";
   }
 
-  mysql://b8381e8687357a:246b0cb1@us-cdbr-east-06.cleardb.net/heroku_c41210addaccbb7?reconnect=true
+
 ?> 
