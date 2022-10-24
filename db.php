88153389fe3147e9578abee53bc73c94ee2576a0
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-24 11:56:08 +0100

    rr

--- db.php
+++ db.php
@@ -1,25 +1,11 @@
-<!-- <?php
-// $connection = mysqli_connect("localhost","root","","class_contact_mgt");
-
-
-// // Check connection
-// if ($connection->connect_error) {
-//     die("Connection failed: " . $connection->connect_error);
-//     echo "Unsuccessful connnection";
-//   }
-
-    
-?>  -->
-
 <?php
-// Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+$connection = mysqli_connect("us-cdbr-east-06.cleardb.net","b9e2f55baf0395","e3e1ccc3","heroku_28ad1c02dc3433c");
+
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+    echo "Unsuccessful connnection";
+  }
+
+?> 
