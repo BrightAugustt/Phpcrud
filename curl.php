38419c553319b96f93f8caf73714ef962456e54b
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-26 18:04:56 +0100

    a

--- curl.php
+++ curl.php
@@ -1,12 +1,17 @@
-
 <?php
+// Create a curl handle
+$ch = curl_init('https://contactlab.herokuapp.com/ajax.php/');
 
-$url = "https://contactlab.herokuapp.com/view.php";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-$result = curl_exec($ch);
-echo $result
+// Send HTTP request and follow redirections
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_exec($ch);
 
+// Get the last effective URL
+$effective_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+
+// Decode the URL
+$effective_url_decoded = curl_unescape($ch, $effective_url);
+
+// Close the handle
+curl_close($ch);
 ?>
-
